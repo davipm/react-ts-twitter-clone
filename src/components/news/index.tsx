f0df427b0378d63da108402bc1403176ab96781c
davi <davi.p.m94@gmail.com>
2020-09-30 19:30:00 -0300

    adding news component

---
+++ src/components/news/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+function News() {
+  return (
+    <Container>
+      <span>Test</span>
+      <strong>Test</strong>
+    </Container>
+  );
+}
+
+export default News;
---
+++ src/components/news/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+
+  > span {
+    margin-bottom: 3px;
+    color: var(--gray);
+  }
+`;
